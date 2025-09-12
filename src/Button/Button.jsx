f2abc5208f6b196bafcd1b7870ce57b75ff84c04
Ihor <iv4enkoigor@gmail.com>
2025-09-12 16:33:00 +0300

    fix Button

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,8 +1,11 @@
+import useTelegram from '../hooks/useTelegram';
 import s from './Button.module.css';
 
-const Button = ({ onClick }) => {
+const Button = () => {
+  const { onClose } = useTelegram;
+
   return (
-    <button onClick={onClick} type="button" className={s.button}>
+    <button onClick={onClose} type="button" className={s.button}>
       Закрити
     </button>
   );
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,10 +3,11 @@ import useTelegram from '../hooks/useTelegram';
 import s from './Header.module.css';
 
 const Header = () => {
-  const { onClose, user } = useTelegram;
+  const { user } = useTelegram;
+
   return (
     <header className={s.header}>
-      <Button onClick={onClose} />
+      <Button />
       <span className={s.username}>{user?.username}</span>
     </header>
   );
